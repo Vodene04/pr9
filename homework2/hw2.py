7ab2ff8fb681b812c473d77437850f2b1c76c306
Vodene04 <a.kamalov2004@gmail.com>
2024-12-24 18:51:40 +0500

    Add files via upload

---
+++ homework2/hw2.py
@@ -0,0 +1,24 @@
+def squares(a, b):
+    try:
+        a = float(a)
+        b = float(b)
+
+        start = min(a, b)
+        end = max(a, b)
+        start = int (start)
+        if end > int (end):
+            end = int (end)
+            squares = [i ** 2 for i in range(start + 1, end + 1)]
+        else:
+            end = int (end)
+            squares = [i ** 2 for i in range(start + 1, end)]
+
+        return squares
+    except ValueError:
+        return "Ошибка ввода: введите числовые значения."
+
+a = input("Введите первое число: ")
+b = input("Введите второе число: ")
+
+result = squares(a, b)
+print(result)
